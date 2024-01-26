19277ab99eb09279038a8587ac9d519898ef9630
Mohaek <elkasmimoha@gmail.com>
2024-01-26 17:01:20 +0100

    Angular Init

---
+++ src/app/furmulario/furmulario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-furmulario',
+  standalone: true,
+  imports: [],
+  templateUrl: './furmulario.component.html',
+  styleUrl: './furmulario.component.css'
+})
+export class FurmularioComponent {
+
+}
