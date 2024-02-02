da2b00a211cf2acd0bcc76123148a2a5e2edbc61
Mohaek <elkasmimoha@gmail.com>
2024-02-02 07:47:46 +0100

    Panaderia completo en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FurmularioComponent} from "./furmulario/furmulario.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FurmularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Panaderia';
+  
 }
--- src/app/furmulario/furmulario.component.ts
+++ src/app/furmulario/furmulario.component.ts
@@ -1,12 +1,130 @@
-import { Component } from '@angular/core';
+import {Component, computed, EventEmitter, signal} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-furmulario',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './furmulario.component.html',
   styleUrl: './furmulario.component.css'
 })
 export class FurmularioComponent {
 
+  productos = [{
+    id: 0,
+    nombre: 'Pan',
+    precio: 2,
+  }, {
+    id: 1,
+    nombre: 'Tarta',
+    precio: 10,
+  }, {
+    id: 2,
+    nombre: 'Bizcocho',
+    precio: 5,
+  }, {
+    id: 3,
+    nombre: 'Galletas',
+    precio: 3,
+  }, {
+    id: 4,
+    nombre: 'Magdalenas',
+    precio: 1,
+  }, {
+    id: 5,
+    nombre: 'Croissant',
+    precio: 2,
+  }, {
+    id: 6,
+    nombre: 'Palmeras',
+    precio: 6,
+  }, {
+    id: 7,
+    nombre: 'Donuts',
+    precio: 2.5,
+  }, {
+    id: 8,
+    nombre: 'Bollos',
+    precio: 3,
+  }, {
+    id: 9,
+    nombre: 'Muffins',
+    precio: 4,
+  }, {
+    id: 10,
+    nombre: 'Rosquillas',
+    precio: 2,
+  },
+  ]
+  productoSeleccionado = signal(this.productos[0].id)
+
+  productoNuevo(id: string) {
+    this.productoSeleccionado.set(Number(id))
+  }
+
+  cantidad = signal(1)
+
+
+  cantidadNueva(cantidad: string) {
+    this.cantidad.set(Number(cantidad))
+  }
+
+  precioProducto = computed(() => {
+    const producto = this.productos
+      .find(producto =>
+        producto.id === this.productoSeleccionado())
+    console.log(producto?.precio)
+    console.log(this.cantidad)
+    return producto ? producto.precio : 0
+  })
+  precioTotal = computed(() => this.cantidad() * this.precioProducto())
+  validarFormulario = computed(() => this.precioTotal() > 0)
+
+
+  productosEnCarrito: Pedido[] = []
+
+  generarTabla(datos: SubmitEvent) {
+    datos.preventDefault()
+    const producto = this.productos
+      .find(producto =>
+        producto.id === this.productoSeleccionado())
+    const pedido = new Pedido()
+    pedido.id = producto?.id || 0
+    pedido.nombre = producto?.nombre || ''
+    pedido.precio = producto?.precio || 0
+    pedido.cantidad = this.cantidad()
+    pedido.precioTotal = this.precioTotal()
+    this.productosEnCarrito.push(pedido)
+    console.log(this.productosEnCarrito)
+
+  }
+
+  eliminarProducto(id: number) {
+    this.productosEnCarrito.splice(id, 1)
+  }
+
+  imprimirTicket() {
+    //Muestras los datos del pedido y el total de todos los pedidos en un alert
+    let total = 0
+    let ticket = ''
+    this.productosEnCarrito.forEach((producto) => {
+      ticket += `Producto: ${producto.nombre} Cantidad: ${producto.cantidad} Precio: ${producto.precioTotal}\n`
+      total += producto.precioTotal
+    })
+    ticket += `Total: ${total}`
+    alert(ticket)
+
+
+  }
+
+}
+
+class Pedido {
+  id: number = 0;
+  nombre: string = '';
+  precio: number = 0;
+  cantidad: number = 0;
+  precioTotal: number = 0;
 }
